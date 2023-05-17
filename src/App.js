4e920e84e20bbb2747ae9c296d00703ead84dca4
salapuLaxmisrinivas <you@example.com>
2023-05-17 11:48:09 +0000

    ROCK PAPER SCISSORS

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/extensions
+// eslint-disable-next-line import/no-unresolved
+import Home from './components/Home'
+
+import './App.css'
+
+const App = () => <Home />
+
+export default App
